bb0bd24a2a871d37c3906d84845b2e6156eb0172
DasBabyPixel <43410952+DasBabyPixel@users.noreply.github.com>
2023-04-12 21:08:37 +0200

    v1

---
+++ src/main/java/de/dasbabypixel/annotations/Api.java
@@ -0,0 +1,13 @@
+package de.dasbabypixel.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD, ElementType.FIELD, ElementType.ANNOTATION_TYPE,
+		ElementType.CONSTRUCTOR, ElementType.TYPE})
+@Retention(RetentionPolicy.SOURCE)
+public @interface Api {
+	String since() default "";
+}
